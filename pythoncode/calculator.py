7f66f14ca0d8ecdaef189d2a2c3fcadfa9646a23
Fiona Yuan <Fiona.Yuan@aspiraconnect.com>
2020-10-18 13:49:42 +0800

    first upload file

---
+++ pythoncode/calculator.py
@@ -0,0 +1,13 @@
+# be tested code
+class Calculator:
+    def add(self, a, b):
+        return a+b
+
+    def min(self, a, b):
+        return a - b
+
+    def cheng(self, a, b):
+        return a * b
+
+    def div(self, a, b):
+        return a / b
